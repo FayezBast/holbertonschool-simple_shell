a158d2d7b22d6e008751f2c654b3b34821e942a0
fayez bast <fayezbast15@gmai.com>
2025-02-17 07:32:07 +0000

    shellpaths

--- shell.c
+++ shell.c
@@ -5,21 +5,55 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <errno.h>
-#include <ctype.h>
 
 #define PROMPT "$fb "
 #define MAX_ARGS 64
-#define MAX_PATH 1024
 
-char *find_command(const char *command)
+char *get_full_path(char *cmd)
 {
     struct stat st;
-    char full_path[MAX_PATH];
+    char *path = NULL;
+    char *path_copy = NULL;
+    char *dir = NULL;
+    char *full_path = NULL;
     
-    if (stat(command, &st) == 0)
-        return strdup(command);
+    if (strchr(cmd, '/') != NULL)
+    {
+        if (stat(cmd, &st) == 0)
+            return strdup(cmd);
+        return NULL;
+    }
+
+    path = getenv("PATH");
+    if (!path)
+        return NULL;
+
+    path_copy = strdup(path);
+    if (!path_copy)
+        return NULL;
+
+    dir = strtok(path_copy, ":");
+    while (dir != NULL)
+    {
+        full_path = malloc(strlen(dir) + strlen(cmd) + 2);
+        if (!full_path)
+        {
+            free(path_copy);
+            return NULL;
+        }
+        sprintf(full_path, "%s/%s", dir, cmd);
         
+        if (stat(full_path, &st) == 0)
+        {
+            free(path_copy);
+            return full_path;
+        }
+        
+        free(full_path);
+        dir = strtok(NULL, ":");
+    }
+    
+    free(path_copy);
     return NULL;
 }
 
@@ -28,38 +62,44 @@ void execute_command(char *cmd)
     char *args[MAX_ARGS];
     char *token;
     int i = 0;
-    char *command_path;
+    char *full_path;
     pid_t pid;
 
-    token = strtok(cmd, " \t\n");
+    while (*cmd == ' ' || *cmd == '\t')
+        cmd++;
+
+    if (*cmd == '\0')
+        return;
+
+    token = strtok(cmd, " \t");
     while (token != NULL && i < MAX_ARGS - 1)
     {
         args[i++] = token;
-        token = strtok(NULL, " \t\n");
+        token = strtok(NULL, " \t");
     }
     args[i] = NULL;
 
     if (args[0] == NULL)
         return;
 
-    command_path = find_command(args[0]);
-    if (!command_path)
+    full_path = get_full_path(args[0]);
+    if (!full_path)
         return;
 
     pid = fork();
     if (pid == 0)
     {
-        execve(command_path, args, NULL);
+        execve(full_path, args, NULL);
         perror("execve");
-        free(command_path);
+        free(full_path);
         exit(EXIT_FAILURE);
     }
     else if (pid > 0)
     {
         wait(NULL);
     }
-    
-    free(command_path);
+
+    free(full_path);
 }
 
 int main(void)
@@ -88,6 +128,7 @@ int main(void)
             break;
         }
 
+        line[strcspn(line, "\n")] = '\0';
         command = strtok(line, ";");
         while (command != NULL)
         {

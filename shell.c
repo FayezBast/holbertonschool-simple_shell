ec4dfd05718703513928ff35bb98e022668dff3d
fayez bast <fayezbast15@gmai.com>
2025-02-17 08:02:45 +0000

    shellpaths

--- shell.c
+++ shell.c
@@ -4,67 +4,16 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <ctype.h>
 #include <errno.h>
 
 #define PROMPT "$fb "
 #define MAX_ARGS 64
-#define MAX_PATH_LEN 1024
-
-char *trim_whitespace(char *str)
-{
-    while (isspace(*str)) str++;
-    if (*str == '\0') return str;
-
-    char *end = str + strlen(str) - 1;
-    while (end > str && isspace(*end)) end--;
-    end[1] = '\0';
-    return str;
-}
-
-char *find_command(const char *cmd)
-{
-    if (cmd == NULL) return NULL;
-
-    if (cmd[0] == '/' || (cmd[0] == '.' && (cmd[1] == '/' || (cmd[1] == '.' && cmd[2] == '/'))))
-    {
-        if (access(cmd, X_OK) == 0) return strdup(cmd);
-        return NULL;
-    }
-
-    char *path = getenv("PATH");
-    if (path == NULL) return NULL;
-
-    char *path_copy = strdup(path);
-    if (path_copy == NULL) return NULL;
-
-    char *dir = strtok(path_copy, ":");
-    while (dir != NULL)
-    {
-        char full_path[MAX_PATH_LEN];
-        snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd);
-
-        if (access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return strdup(full_path);
-        }
-
-        dir = strtok(NULL, ":");
-    }
-
-    free(path_copy);
-    return NULL;
-}
 
 void execute_command(char *cmd)
 {
     char *args[MAX_ARGS];
     char *token;
     int i = 0;
-
-    cmd = trim_whitespace(cmd);
-    if (cmd[0] == '\0') return;
 
     token = strtok(cmd, " \t");
     while (token != NULL && i < MAX_ARGS - 1)
@@ -74,40 +23,72 @@ void execute_command(char *cmd)
     }
     args[i] = NULL;
 
-    if (args[0] == NULL) return;
+    if (args[0] == NULL)
+        return;
 
-    char *command_path = find_command(args[0]);
-    if (command_path == NULL)
+    if (strchr(args[0], '/') != NULL)
     {
-        fprintf(stderr, "%s: command not found\n", args[0]);
+        if (access(args[0], X_OK) == 0)
+        {
+            if (fork() == 0)
+            {
+                execve(args[0], args, NULL);
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
+            else
+            {
+                wait(NULL);
+            }
+        }
+        else
+        {
+            fprintf(stderr, "Command not found: %s\n", args[0]);
+        }
         return;
     }
 
-    pid_t pid = fork();
-    if (pid == 0)
+    char *path = getenv("PATH");
+    char *path_token = strtok(path, ":");
+    char cmd_path[1024];
+    int found = 0;
+
+    while (path_token != NULL)
     {
-        execve(command_path, args, environ);
+        snprintf(cmd_path, sizeof(cmd_path), "%s/%s", path_token, args[0]);
+        if (access(cmd_path, X_OK) == 0)
+        {
+            args[0] = cmd_path;
+            found = 1;
+            break;
+        }
+        path_token = strtok(NULL, ":");
+    }
+
+    if (!found)
+    {
+        fprintf(stderr, "Command not found: %s\n", args[0]);
+        return;
+    }
+
+    if (fork() == 0)
+    {
+        execve(args[0], args, NULL);
         perror("execve");
         exit(EXIT_FAILURE);
     }
-    else if (pid > 0)
+    else
     {
         wait(NULL);
     }
-    else
-    {
-        perror("fork");
-    }
-
-    free(command_path);
 }
 
 int main(void)
 {
     char *line = NULL;
     size_t len = 0;
-    ssize_t nread;
     int interactive = isatty(STDIN_FILENO);
+    char *command;
 
     while (1)
     {
@@ -117,8 +98,7 @@ int main(void)
             fflush(stdout);
         }
 
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
+        if (getline(&line, &len, stdin) == -1)
         {
             if (interactive)
                 printf("\n");
@@ -127,7 +107,7 @@ int main(void)
 
         line[strcspn(line, "\n")] = '\0';
 
-        char *command = strtok(line, ";");
+        command = strtok(line, ";");
         while (command != NULL)
         {
             execute_command(command);
